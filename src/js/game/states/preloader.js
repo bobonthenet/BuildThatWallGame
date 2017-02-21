6c718b39548d216e1a835842d773837dd11f2043
Robert Bedard <rbedard@plex.com>
2017-02-21 13:07:22 -0500

    Attempt to Fix Audio

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -1,21 +1,21 @@
 var preloader = {};
 
 preloader.preload = function () {
-  this.game.load.image('logo', 'images/phaser.png');
-  this.game.load.atlas('breakout', 'images/breakout.png', 'images/breakout.json');
-  this.game.load.image('seal', 'images/seal.png');
-  this.game.load.atlas('trump', 'images/trump.png', 'images/trump.json');
+  this.game.load.image('logo', './images/phaser.png');
+  this.game.load.atlas('breakout', './images/breakout.png', './images/breakout.json');
+  this.game.load.image('seal', './images/seal.png');
+  this.game.load.atlas('trump', './images/trump.png', './images/trump.json');
 
-  this.game.load.audio('weNeedToBuild', [ 'audio/WeNeedToBuild.ogg', 'audio/WeNeedToBuild.mp3' ]);
+  this.game.load.audio('weNeedToBuild', [ './audio/WeNeedToBuild.ogg', './audio/WeNeedToBuild.mp3' ]);
 
-  this.game.load.audio('smash1', [ 'audio/smash1.ogg', 'audio/smash1.mp3' ]);
-  this.game.load.audio('smash2', [ 'audio/smash2.ogg', 'audio/smash2.mp3' ]);
+  this.game.load.audio('smash1', [ './audio/smash1.ogg', './audio/smash1.mp3' ]);
+  this.game.load.audio('smash2', [ './audio/smash2.ogg', './audio/smash2.mp3' ]);
 
   //Hit Trump sounds.
-  this.game.load.audio('beef', [ 'audio/beef.ogg', 'audio/beef.mp3' ]);
-  this.game.load.audio('stopIt', [ 'audio/stopit.ogg', 'audio/stopit.mp3' ]);
-  this.game.load.audio('cameoutofnowhere', [ 'audio/cameoutofnowhere.ogg', 'audio/cameoutofnowhere.mp3' ]);
-  this.game.load.audio('stupidpeopleinourcountry', [ 'audio/stupidpeopleinourcountry.ogg', 'audio/stupidpeopleinourcountry.mp3' ]);
+  this.game.load.audio('beef', [ './audio/beef.ogg', './audio/beef.mp3' ]);
+  this.game.load.audio('stopIt', [ './audio/stopit.ogg', './audio/stopit.mp3' ]);
+  this.game.load.audio('cameoutofnowhere', [ './audio/cameoutofnowhere.ogg', './audio/cameoutofnowhere.mp3' ]);
+  this.game.load.audio('stupidpeopleinourcountry', [ './audio/stupidpeopleinourcountry.ogg', './audio/stupidpeopleinourcountry.mp3' ]);
 
 };
 
